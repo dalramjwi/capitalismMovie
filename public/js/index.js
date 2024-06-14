409addd4f8c7f6491550819dd874beeeda495895
dalramjwi <dalramjwi@gmail.com>
2024-06-14 17:03:20 +0900

    list making page

--- public/js/index.js
+++ public/js/index.js
@@ -15,23 +15,28 @@ setName[1].style.backgroundColor = "red";
 const getMovieAJAX = () => {
   const xhr = new XMLHttpRequest();
   const key = `583ae85ee3d661098b9bed3b3c43ec4b`;
-  const pageNumber = 1;
-  for (let i = 0; i < 6; i++) {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=ko-KR&page=${i}`;
+  function pageCount(page) {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=ko-KR&page=${page}`;
     xhr.open("GET", url);
-  }
-  xhr.addEventListener("load", () => {
-    if (xhr.status === 200) {
-      const result = JSON.parse(xhr.responseText);
-      console.dir(result);
-      console.dir(result.results);
-      result.results.forEach((item) => {
-        console.log(item.title);
-        console.log(item);
-        setName[3].innerHTML += `<li>${item.title}</li>`;
-      });
+    xhr.send();
+    xhr.addEventListener("load", () => {
+      if (xhr.status === 200) {
+        const result = JSON.parse(xhr.responseText);
+        const listArray = [];
+        result.results.forEach((item) => {
+          console.log(item);
+          listArray.push(
+            `<li>제목 : ${item.title} <br> 소개글 : ${item.overview} <br>        <img src="https://image.tmdb.org/t/p/w500/${item.poster_path}" alt="${item.title}poster">
+            </li>`
+          );
+        });
+        setName[3].innerHTML = `<ul>${listArray.join("")}</ul>`;
+      }
+    });
+    if (page < 6) {
+      pageCount(page + 1);
     }
-  });
-  xhr.send();
+  }
+  pageCount(1);
 };
 getMovieAJAX();
