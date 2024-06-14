971bffb266c3da4003d10248098441613297a1ba
dalramjwi <dalramjwi@gmail.com>
2024-06-14 16:25:06 +0900

    finish to use const

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,17 @@
 import { resultVariables } from "./useMakeVariables.js";
-import { tagName } from "./tagName.js";
+const setName = [];
+const setId = [];
+resultVariables().forEach((item) => {
+  setName.push(item._name);
+  setId.push(item._id);
+});
+for (let i = 0; i < setName.length; i++) {
+  setName[i] = document.getElementById(`${setId[i]}`);
+  console.log(setName[i]);
+}
+setName[1].style.backgroundColor = "red";
 
-console.log(resultVariables());
+//AJAX 사용
 const getMovieAJAX = () => {
   const xhr = new XMLHttpRequest();
   const key = `583ae85ee3d661098b9bed3b3c43ec4b`;
@@ -10,8 +20,6 @@ const getMovieAJAX = () => {
   xhr.open("GET", url);
   xhr.addEventListener("load", () => {
     if (xhr.status === 200) {
-      console.log("데이터 조회");
-      console.dir(xhr);
       const result = JSON.parse(xhr.responseText);
       console.dir(result);
       console.dir(result.results);
@@ -20,19 +28,3 @@ const getMovieAJAX = () => {
   xhr.send();
 };
 getMovieAJAX();
-// function a() {
-//   for (let i = 0; i < 5; i++) {
-//     const name = resultVariables()[i]._name;
-//     const id = resultVariables()[i]._id;
-//     const arr = [];
-//     const target = document.getElementById(id);
-//     return target;
-//   }
-// }
-const arr = [];
-tagName.forEach((item) => {
-  arr.push(document.getElementById(item));
-});
-console.log(arr);
-const a = arr[1];
-console.log(0);
