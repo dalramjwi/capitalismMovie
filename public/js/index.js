f1ffcb35607f5d11ee1467a275c5769a882629e5
dalramjwi <dalramjwi@gmail.com>
2024-06-14 12:38:09 +0900

    xhr base write, and not working

--- public/js/index.js
+++ public/js/index.js
@@ -2,4 +2,12 @@ const getMovieAJAX = () => {
   const xhr = new XMLHttpRequest();
   const url = ` https://api.themoviedb.org/3/movie/popular?language=ko&page=1`;
   xhr.open("GET", url);
+  xhr.addEventListener("load", () => {
+    if (xhr.status === 200) {
+      console.log("데이터 조회");
+      console.dir(xhr);
+    }
+  });
+  xhr.send();
 };
+getMovieAJAX();
