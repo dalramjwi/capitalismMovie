eaaa771100ff3e177dfb97013bd0bd562dafc248
dalramjwi <dalramjwi@gmail.com>
2024-06-14 14:18:55 +0900

    update api key writing

--- public/js/index.js
+++ public/js/index.js
@@ -29,13 +29,12 @@ console.log(test);
 const getMovieAJAX = () => {
   const xhr = new XMLHttpRequest();
   const key = `583ae85ee3d661098b9bed3b3c43ec4b`;
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key={${key}}&language=ko-KR`;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=ko-KR&page=1`;
   xhr.open("GET", url);
-
   xhr.addEventListener("load", () => {
     if (xhr.status === 200) {
-      // console.log("데이터 조회");
-      // console.dir(xhr);
+      console.log("데이터 조회");
+      console.dir(xhr);
       const result = JSON.parse(xhr.responseText);
       console.dir(result);
       console.dir(result.results);
