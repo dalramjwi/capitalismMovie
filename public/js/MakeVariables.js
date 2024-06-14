48b1d76b2755f60ead83b482ea01f93c256008ff
dalramjwi <dalramjwi@gmail.com>
2024-06-14 14:26:38 +0900

    MakeVariables function move to another js

---
+++ public/js/MakeVariables.js
@@ -0,0 +1,26 @@
+class MakeVariables {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+  }
+  set name(value) {
+    if (typeof value === "string") {
+      this._name = value;
+    } else {
+      console.error("문자열 아님");
+    }
+  }
+  get name() {
+    return this._name;
+  }
+  set id(value) {
+    if (typeof value === "string") {
+      this._id = value;
+    } else {
+      console.error("문자열 아님");
+    }
+  }
+  get id() {
+    return this._id;
+  }
+}
