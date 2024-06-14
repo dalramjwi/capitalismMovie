5aacf9c6399901fd2e6719692adf092269404767
dalramjwi <dalramjwi@gmail.com>
2024-06-14 13:07:01 +0900

    make makeVariables

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,31 @@
+class MakeVariables {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+  }
+  set name(value) {
+    if (typeof value === "string") {
+      this._name = value;
+    } else {
+      console.error("문자열 아님");
+    }
+  }
+  get name() {
+    return this._name;
+  }
+  set id(value) {
+    if (typeof value === "string") {
+      this._id = value;
+    } else {
+      console.error("문자열 아님");
+    }
+  }
+  get id() {
+    return this._id;
+  }
+}
+let test = new MakeVariables("root", "root");
+console.log(test);
 const getMovieAJAX = () => {
   const xhr = new XMLHttpRequest();
   const key = `583ae85ee3d661098b9bed3b3c43ec4b`;
