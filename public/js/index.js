747e5f58b952f8760cab71c2dd2fe8539decf369
dalramjwi <dalramjwi@gmail.com>
2024-06-14 12:56:41 +0900

    xhr api key write

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,15 @@
 const getMovieAJAX = () => {
   const xhr = new XMLHttpRequest();
-  const url = ` https://api.themoviedb.org/3/movie/popular?language=ko&page=1`;
+  const key = `583ae85ee3d661098b9bed3b3c43ec4b`;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key={${key}}&language=ko-KR`;
   xhr.open("GET", url);
+
   xhr.addEventListener("load", () => {
     if (xhr.status === 200) {
       console.log("데이터 조회");
       console.dir(xhr);
     }
   });
-  xhr.send();
+  xhr.send(data);
 };
 getMovieAJAX();
