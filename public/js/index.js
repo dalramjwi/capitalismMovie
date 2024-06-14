83a1ae6c325e4d2e32dac13b643f061924665993
dalramjwi <dalramjwi@gmail.com>
2024-06-14 17:12:51 +0900

    list complete

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 import { resultVariables } from "./useMakeVariables.js";
+//변수 이름 할당
 const setName = [];
 const setId = [];
 resultVariables().forEach((item) => {
@@ -7,36 +8,30 @@ resultVariables().forEach((item) => {
 });
 for (let i = 0; i < setName.length; i++) {
   setName[i] = document.getElementById(`${setId[i]}`);
-  console.log(setName[i]);
 }
-setName[1].style.backgroundColor = "red";
 
 //AJAX 사용
-const getMovieAJAX = () => {
+const getMovieAJAX = (page) => {
   const xhr = new XMLHttpRequest();
   const key = `583ae85ee3d661098b9bed3b3c43ec4b`;
-  function pageCount(page) {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=ko-KR&page=${page}`;
-    xhr.open("GET", url);
-    xhr.send();
-    xhr.addEventListener("load", () => {
-      if (xhr.status === 200) {
-        const result = JSON.parse(xhr.responseText);
-        const listArray = [];
-        result.results.forEach((item) => {
-          console.log(item);
-          listArray.push(
-            `<li>제목 : ${item.title} <br> 소개글 : ${item.overview} <br>        <img src="https://image.tmdb.org/t/p/w500/${item.poster_path}" alt="${item.title}poster">
-            </li>`
-          );
-        });
-        setName[3].innerHTML = `<ul>${listArray.join("")}</ul>`;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=ko-KR&page=${page}`;
+  xhr.open("GET", url);
+  xhr.addEventListener("load", () => {
+    if (xhr.status === 200) {
+      const result = JSON.parse(xhr.responseText);
+      const listArray = [];
+      result.results.forEach((item) => {
+        listArray.push(
+          `<li>제목 : ${item.title} <br> 소개글 : ${item.overview} <br>        <img src="https://image.tmdb.org/t/p/w500/${item.poster_path}" alt="${item.title}poster">
+          </li>`
+        );
+      });
+      setName[3].innerHTML += `<ul>${listArray.join("")}</ul>`;
+      if (page < 8) {
+        getMovieAJAX(page + 1);
       }
-    });
-    if (page < 6) {
-      pageCount(page + 1);
     }
-  }
-  pageCount(1);
+  });
+  xhr.send();
 };
-getMovieAJAX();
+getMovieAJAX(1);
