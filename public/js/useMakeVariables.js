2b94dacdac04ec74c5feb88034d1495ee07b2064
dalramjwi <dalramjwi@gmail.com>
2024-06-14 17:59:49 +0900

    useMake comment

--- public/js/useMakeVariables.js
+++ public/js/useMakeVariables.js
@@ -1,5 +1,8 @@
 import { MakeVariables } from "./MakeVariables.js";
 import { tagName } from "./tagName.js";
+/**
+ * 배열을 이용해 객체를 만드는 함수
+ */
 export function resultVariables() {
   const arr = [];
   tagName.forEach((item) => {
